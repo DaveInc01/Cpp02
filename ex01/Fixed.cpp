75ec94024c884f7d8b525f5c8a41e9dc32e8332f
Areeviik <arevik.xachaturyan@gmail.com>
2023-07-10 20:43:13 +0400

    ex in process

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -5,18 +5,6 @@ Fixed::Fixed(){
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const int x)
-{
-    this->x = x;
-    std::cout << "Int constructor called" << std::endl;
-}
-
-Fixed::Fixed(const float x)
-{
-    this->x = x;
-    std::cout << "Float constructor called" << std::endl;
-}
-
 Fixed::Fixed(Fixed &obj)
 {
     std::cout << "Copy constructor called" << std::endl;
--- ex01/includes/Fixed.hpp
+++ ex01/includes/Fixed.hpp
@@ -8,13 +8,10 @@ class Fixed
 public:
     Fixed( void );
     Fixed( Fixed& obj );
-    Fixed( const int x );
-    Fixed( const float x );
     ~Fixed();
     void operator = (const Fixed& obj);
     int  getRawBits( void ) const;
     void setRawBits( int const raw );
-    float toFloat( void ) const;
 private:
     int x;
     static const int w;
